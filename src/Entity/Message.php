b98a207d6c071c567eaf17875738f09a009f2a8a
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-07 10:56:13 +0200

    feat: created Message entity

---
+++ src/Entity/Message.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="messages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Trick::class, inversedBy="messages")
+     */
+    private $post;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $post_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $last_update;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPost(): ?Trick
+    {
+        return $this->post;
+    }
+
+    public function setPost(?Trick $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPostDate(): ?\DateTimeInterface
+    {
+        return $this->post_date;
+    }
+
+    public function setPostDate(\DateTimeInterface $post_date): self
+    {
+        $this->post_date = $post_date;
+
+        return $this;
+    }
+
+    public function getLastUpdate(): ?\DateTimeInterface
+    {
+        return $this->last_update;
+    }
+
+    public function setLastUpdate(\DateTimeInterface $last_update): self
+    {
+        $this->last_update = $last_update;
+
+        return $this;
+    }
+}
