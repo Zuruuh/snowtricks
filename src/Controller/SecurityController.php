b8f4cf8e9a3e06521cc2ceebfb6c502604d03958
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-09 08:35:09 +0200

    feat: created security controller for login and logout routes

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+
+class SecurityController extends AbstractController
+{
+
+    public function __construct(FlashBagInterface $flash)
+    {
+        $this->flash = $flash;
+    }
+
+    
+    #[Route("/login", name: "app_login")]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            $this->flash->add("warning", "You are already connected !");
+            return $this->redirectToRoute('home.index');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    
+    #[Route("/logout", name: "app_logout")]
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank');
+    }
+}
