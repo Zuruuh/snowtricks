27e6d36b427dcbb0efa6d3ea49df438647f5b835
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-09 06:43:34 +0200

    feat: automatic dependencies related changes

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
