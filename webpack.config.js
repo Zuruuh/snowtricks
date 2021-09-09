80382966411184f9a9250f0a104e3810991f348a
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-09 08:32:38 +0200

    feat: added toast script to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('forms', './assets/scripts/components/forms.js')
+    .addEntry('toasts', './assets/scripts/components/toasts.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
