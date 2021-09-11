6c3a09be171f2e86ab1b47f578df8dec3290ed5d
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-11 03:40:24 +0200

    feat: defined email as unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $email;
 
