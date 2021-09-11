2934a0ff07c381051a0c591a47a9d9e7219ef506
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-11 03:42:14 +0200

    feat: created FileService to facilitate files interactions

---
+++ src/Services/FileService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Filesystem\Filesystem;
+
+class FileService
+{
+    public function __construct()
+    {
+        $this->fileSystem = new Filesystem();
+    }
+
+    public function createFolder(string $folder): string
+    {
+        if (!$this->fileSystem->exists(getcwd() . $folder)) {
+            $this->fileSystem->mkdir(getcwd() . $folder);
+        }
+        return getcwd() . $folder;
+    }
+
+    public function move(UploadedFile $file, string $path, string $name): string
+    {
+        $file->move(getcwd() . $path, $name);
+        return $path . $name;
+    }
+}
