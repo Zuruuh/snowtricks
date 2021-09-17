d88c9974568bf9bd8daebf4546c2e285013bca49
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-17 15:40:12 +0200

    feat: updated trick form to be unmapped on certain fields

--- src/Form/TrickFormType.php
+++ src/Form/TrickFormType.php
@@ -6,43 +6,31 @@ use App\Entity\Trick;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class TrickFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Trick\'s name',
-                "required" => true,
-            ])
-            ->add('overview', TextType::class, [
-                'label' => 'Short description'
-            ])
-            ->add('description', TextareaType::class, [
-                'label' => 'Trick\'s Content',
-                "required" => true,
-            ])
-            ->add('thumbnail', FileType::class, [
-                "required" => false
-            ]) // // / TODO Add thumbnail uploading
-            ->add('images', FileType::class, [ // // / TODO Add images uploading for images
-                'multiple' => true,
-                'required' => false
-            ])
+            ->add('name')
+            ->add('description')
+            ->add('overview')
             ->add('category')
-
-            // // / TODO Add dynamic fields for videos:
-            // Let users add or remove fields by clicking on the +/- buttons
-            ->add('videos', TextType::class, [
-                "required" => false,
-                "attr" => [
-                    "value" => "[]",
-                ]
+            ->add('thumbnail', FileType::class, [
+                'required' => false,
+                'mapped' => false
+            ])
+            ->add('images', FileType::class, [
+                'required' => false,
+                'multiple' => true,
+                'mapped' => false    
+            ])
+            ->add('videos', TextareaType::class, [
+                'required' => false,
+                'mapped' => false
             ])
         ;
     }
