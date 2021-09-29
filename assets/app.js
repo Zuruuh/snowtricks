3fdd7bf4870abea042c880101ee766048eea2f30
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-30 00:55:48 +0200

    feat: configured swiper

--- assets/app.js
+++ assets/app.js
@@ -7,11 +7,32 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // assets imports
 
 // bootstrap imports
 import { Tooltip, Toast, Popover, Modal } from "bootstrap";
+import Swiper, { Navigation, Pagination } from "swiper";
+
+Swiper.use([Navigation, Pagination]);
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  allowTouchMove: false,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 // start the Stimulus application
 import "./bootstrap";
