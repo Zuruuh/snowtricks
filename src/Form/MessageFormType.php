ed4991e5d5bee02c60ffbf2719346dab0505b315
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-13 00:39:07 +0200

    feat: added constraints to message form

--- src/Form/MessageFormType.php
+++ src/Form/MessageFormType.php
@@ -4,17 +4,29 @@ namespace App\Form;
 
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MessageFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', TextareaType::class)
-        ;
+            ->add('content', TextareaType::class, [
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Your comment can\'t be blank !'
+                    ]),
+                    new Length([
+                        'max' => 2048,
+                        'maxMessage' => 'Your comment can\'t be more than {{ limit }} characters !'
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
