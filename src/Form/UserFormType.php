8a8f834daea3d25763d266a867a6ebddfc42acfb
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-14 19:46:00 +0200

    feat: added constraint to profile_picture field

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -3,10 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Service\TrickService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class UserFormType extends AbstractType
 {
@@ -17,8 +19,13 @@ class UserFormType extends AbstractType
                 'label' => 'Profile picture',
                 'mapped' => false,
                 'required' => false,
-            ])
-        ;
+                'constraints' => [
+                    new Image([
+                        'mimeTypes' => TrickService::SUPPORTED_FORMATS,
+                        'mimeTypesMessage' => TrickService::INVALID_FORMAT
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
